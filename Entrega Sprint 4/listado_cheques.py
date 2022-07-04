5dbe73b5111a70ac7a0ce937b867f2346529ad46
EzeqRodriguez <105322427+EzeqRodriguez@users.noreply.github.com>
2022-07-04 03:40:20 -0300

    Update listado_cheques.py

--- Entrega Sprint 4/listado_cheques.py
+++ Entrega Sprint 4/listado_cheques.py
@@ -1,19 +1,22 @@
 import csv
 
+
 def buscarPorDni():
-    
-    dni=input("Ingrese su dni: ")
-    
-    datos = csv.reader(open("test.csv"))
-    
+
+    dni = input("Ingrese su dni: ")
+
+    datos = csv.DictReader(open("test.csv"))
+
     for linea in datos:
-        if dni == linea[8]:
-            print(f'\n El valor del cheque es de: {linea[5]} - Su numero de cheque es: {linea[0]} - El estado es: {linea[9]} - El codigo de sucursal es: {linea[2]}')
+        if linea["DNI"] == dni:
+            print(linea)
         else:
-            print("Error su dni no esta en nuestra base de datos, intente nuevamente")       
-        
+            print("El dni no se encuentra en nuestra base de datos")
+        break
+
 
 buscarPorDni()
-
+buscarPorDni()
+buscarPorDni()
 
 
